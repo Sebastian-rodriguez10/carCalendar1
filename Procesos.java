1208f7584b68a4276fd1104e3c8a7a00d22c8b6c
Juan Andrés Felipe Castro Londoño <juanandresfelipec@gmail.com>
2026-02-16 21:18:33 -0500

    Clase de ¨Procesos.

--- Procesos.java
+++ Procesos.java
@@ -56,24 +56,37 @@ public class Procesos {
         registro.add(evento);
     }
 
-    public void getEventos() {
-        
+    public void imprimirEventos() {
+        if (registro.size() == 0) {
+            System.out.println("\nNO SE PUEDE HACER LA CONSULTA. NOHAY EVENTOS REGISTRADOS");
+        } else {
+            System.out.println("\nLos eventos que usted, Carlos, ha ingresado son los siguientes: ");
+            System.out.println();
+            for(int i = 0; i < registro.size(); i++){
+                System.out.print("ID del evento: " + registro.get(i).getId());
+                System.out.print("Nombre del evento: " + registro.get(i).getNombre());
+                System.out.print("Descripción del evento: " + registro.get(i).getDescripcion());
+                System.out.print("Inicio del evento: " + registro.get(i).getInicio());
+                System.out.print("Nombre del evento: " + registro.get(i).getFin());
+                System.out.println();
+            }
+        }
     }
 
-    public void buscarProducto(int id) {
+    public void buscarEvento(int id) {
         int acumulador = 0;
         boolean valor = false;
         do {
-            if (registro.get(acumulador).getId() == id) {
-                System.out.println("\nDATOS DEL PRODUCTO:");
-                System.out.println("-El ID del producto: " + registro.get(acumulador).getId() + ".");
-                System.out.println("-Nombre del producto: " + registro.get(acumulador).getNombre() + ".");
-                System.out.println("-Precio: $" + registro.get(acumulador).getPrecio() + " COP.");
-                System.out.println("-Cantidad en Stock: " + registro.get(acumulador).getCantidadEnStock() + ".");
-                System.out.println("-Característica: " + registro.get(acumulador).caracteristica() + ".");
+            if (acumulador == registro.size()) {
+                System.out.println("\nNO SE ENCONTRÓ UN EVENTO QUE TUVIERÁ EL NÚMERO DEL ID DIGITADO.");
                 valor = true;
-            } else if (acumulador == registro.size()) {
-                System.out.println("\nNO SE ENCONTRÓ UN PRODUCTO QUE TUVIERÁ EL NÚMERO DEL ID DIGITADO.");
+            } else if (registro.get(acumulador).getId() == id) {
+                System.out.println("\nDATOS DEL EVENTO:");
+                System.out.println("-El ID del evento: " + registro.get(acumulador).getId() + ".");
+                System.out.println("-Nombre del evento: " + registro.get(acumulador).getNombre() + ".");
+                System.out.println("-Descripción" + registro.get(acumulador).getDescripcion() + " .");
+                System.out.println("-Inicio: " + registro.get(acumulador).getInicio() + ".");
+                System.out.println("-Fin: " + registro.get(acumulador).getFin() + ".");
                 valor = true;
             } else {
                 acumulador++;
@@ -81,39 +94,55 @@ public class Procesos {
         } while (!valor);
     }
 
-    public void actualizarProducto(int id) {
+    public void actualizarEvento(int id) {
         int acumulador = 0;
         boolean valor = false;
         do {
-            if (registro.get(acumulador).getId() == id) {
-                System.out.println("\nDATOS ACTUALES DEL PRODUCTO:");
-                System.out.println("-El ID del producto: " + registro.get(acumulador).getId() + ".");
-                System.out.println("-Nombre del producto: " + registro.get(acumulador).getNombre() + ".");
-                System.out.println("-Precio: $" + registro.get(acumulador).getPrecio() + " COP.");
-                System.out.println("\nUsuario, por favor, ingrese los nuevos datos referentes al producto actual: ");
+            if (acumulador == registro.size()) {
+                System.out.println("\nNO SE ENCONTRÓ UN PRODUCTO QUE TUVIERÁ EL NÚMERO DEL ID DIGITADO.");
+                valor = true;
+            } else if (registro.get(acumulador).getId() == id) {
+                System.out.println("\nDATOS ACTUALES DEL EVENTO:");
+                System.out.println("-El ID del evento: " + registro.get(acumulador).getId() + ".");
+                System.out.println("-Nombre del evento: " + registro.get(acumulador).getNombre() + ".");
+                System.out.println("-Descripción: " + registro.get(acumulador).getDescripcion() + ".");
+                System.out.println("-Inicio: " + registro.get(acumulador).getInicio() + ".");
+                System.out.println("-Fin: " + registro.get(acumulador).getFin() + ".");
+                System.out.println("\nUsuario, por favor, ingrese los nuevos datos referentes al evento actual: ");
                 System.out.print("-Nuevo nombre: ");
                 String nombre = sc.nextLine();
                 registro.get(acumulador).setNombre(nombre);
-                System.out.print("-Precio nuevo: $");
-                double precio = sc.nextDouble();
-                while (precio <= 0) {
-                    System.out.println("\nVALOR DE PRECIO INVALIDO.");
-                    System.out.print("Por favor, ingrese en números, un precio mayor a $0 COP: ");
-                    precio = sc.nextDouble();
-                }
-                registro.get(acumulador).setPrecio(precio);
-                System.out.println("-Actualización de cantidad en Stock (por favor ingresar números): ");
-                int cantidad = sc.nextInt();
-                while (cantidad <= 0) {
-                    System.out.println("\nDATO INVALIDO.");
-                    System.out.print("Por favor, ingrese en un número mayor a 0 la cantidad del prodcuto en el stock: ");
-                    cantidad = sc.nextInt();
-                }
-                registro.get(acumulador).setCantidadEnStock(cantidad);
+                System.out.print("-Nueva descripción: ");
+                String descripcion = sc.nextLine();
+                registro.get(acumulador).setDescripcion(descripcion);
+                System.out.println("-Nuevo inicio: ");
+                System.out.print("- Año (en números): ");
+                int año = sc.nextInt();
+                System.out.print("- Mes (en números): ");
+                int mes = sc.nextInt();
+                System.out.print("- Día del mes (en números): ");
+                int dia = sc.nextInt();
+                System.out.print("- Hora (en números): ");
+                int horas = sc.nextInt();
+                System.out.print("- minutos (en números): ");
+                int minutos = sc.nextInt();
+                LocalDateTime inicio = LocalDateTime.of(año, mes, dia, horas, minutos);
+                registro.get(acumulador).setInicio(inicio);;
+                System.out.println();
+                System.out.println("Ingrese los datos del nuevo fin del evento: ");
+                System.out.print("- Año (en números): ");
+                int añoF = sc.nextInt();
+                System.out.print("- Mes (en números): ");
+                int mesF = sc.nextInt();
+                System.out.print("- Día del mes (en números): ");
+                int diaF = sc.nextInt();
+                System.out.print("- Hora (en números): ");
+                int horasF = sc.nextInt();
+                System.out.print("- Minutos (en números): ");
+                int minutosF = sc.nextInt();
+                LocalDateTime fin = LocalDateTime.of(añoF, mesF, diaF, horasF, minutosF);                
+                registro.get(acumulador).setFin(fin);
                 System.out.println("\nSE HA REALIZADO EL CAMBIO EFECTIVAMENTE.");
-                valor = true;
-            } else if (acumulador == registro.size()) {
-                System.out.println("\nNO SE ENCONTRÓ UN PRODUCTO QUE TUVIERÁ EL NÚMERO DEL ID DIGITADO.");
                 valor = true;
             } else {
                 acumulador++;
@@ -121,14 +150,17 @@ public class Procesos {
         } while (!valor);    
     }
 
-    public void eliminarProducto(int id) {
+    public void eliminarEvento(int id) {
         int acumulador = 0;
         boolean valor = false;
         do {
-            if (registro.get(acumulador).getId() == id) {
-                System.out.println("\nDATOS DEL PRODUCTO QUE SE QUIERE ELIMINAR:");
-                System.out.println("-El ID del producto: " + registro.get(acumulador).getId() + ".");
-                System.out.println("-Nombre del producto: " + registro.get(acumulador).getNombre() + ".");
+            if (acumulador == registro.size()) {
+                System.out.println("\nNO SE ENCONTRÓ UN EVENTO CON EL ID INGRESADO.");
+                valor = true;                 
+            } else if (registro.get(acumulador).getId() == id) {
+                System.out.println("\nDATOS DEL EVENTO QUE SE QUIERE ELIMINAR:");
+                System.out.println("-El ID del evento: " + registro.get(acumulador).getId() + ".");
+                System.out.println("-Nombre del evento: " + registro.get(acumulador).getNombre() + ".");
                 System.out.print("\nUsuario, ¿está seguro de que quiere eleiminar este producto? (Escriba 'si' o 'no': ");
                 String decision = sc.nextLine();
                 while (!decision.equalsIgnoreCase("si") || !decision.equalsIgnoreCase("no")) {
@@ -138,15 +170,12 @@ public class Procesos {
                 }
                 if (decision.equalsIgnoreCase("si")) {
                     registro.remove(acumulador);
-                    System.out.println("\nSE HA ELIMINADO EL PRODCUTO EFECTIVAMENTE.");
+                    System.out.println("\nSE HA ELIMINADO EL EVENTO EFECTIVAMENTE.");
                     valor = true;
                 } else if (decision.equalsIgnoreCase("no")) {
                     System.out.println("\nSE HA CANCELADO LA ELIMINACIÍON.");
                     valor = true;
-                }                
-            } else if (acumulador == registro.size()) {
-                System.out.println("\nNO SE ENCONTRÓ UN PRODUCTO CON EL ID INGRESADO.");
-                valor = true;
+                }           
             } else {
                 acumulador++;
             }
